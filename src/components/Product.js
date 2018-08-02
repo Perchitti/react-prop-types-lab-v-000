d9f4c54ec187821a53f4bb0412f03c283409d61c
Lauren Perchitti <laurenperchitti@gmail.com>
2018-08-02 00:28:12 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,3 +1,15 @@
 // Code Product Component Here
 import React from 'react'
 import PropTypes from 'prop-types'
+
+class Product extends React.Component {
+  render() {
+    return (
+      <div className="product">
+      <ul>
+      <li>
+      </ul>
+      </div>
+    )
+  }
+}
